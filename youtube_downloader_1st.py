49f9f01cfad4a0e9b6d8f80e2d60f3f9b8758391
TheHazem <82238761+TheHazem@users.noreply.github.com>
2021-08-06 14:33:37 +0200

    Update youtube_downloader_1st.py

--- youtube_downloader_1st.py
+++ youtube_downloader_1st.py
@@ -2,14 +2,23 @@ from pytube import YouTube
 
 YT = YouTube(input("-- Please copy and paste the video URl here --"))
 
-# print(YT.title)
+print(YT.title)
 
-# print(YT.thumbnail_url)
+print(YT.thumbnail_url)
 
-# print(YT.description)
+print(YT.description)
 
-# print(YT.views)
-
-
-
-YT.streams.get_lowest_resolution().download(filename="HAHAHAHA")
+print(YT.views)
+# to get the lowest youtube video resolution
+YT.streams.get_lowest_resolution().download(filename="lowest")
+# ##################################################################
+# to get video title
+print(YT.title)
+# to get video thumbnail photo
+print(YT.thumbnail_url)
+# to get video description
+print(YT.description)
+# to get video views
+print(YT.views)
+# to get the highest youtube video resolution
+YT.streams.get_highest_resolution().download(filename="High")
